b64b547e6f8d03f6355e510d9e2d8c841bbe7b3c
Habib <habnacos@gmail.com>
2018-11-26 04:23:13 -0500

    interfaz

---
+++ Tabla.py
@@ -0,0 +1,196 @@
+from sympy import Symbol, Rational, Poly
+
+class Matriz():
+    def __init__(self, fo, restricciones):
+        self.pivote = [0, 0]
+        self.fo = fo                                    # Función objetivo.
+        self.restricciones = restricciones              # Restricciones.
+        self.num_res = len(restricciones)               # Número de restricciones.
+        self.num_var = len(fo.coef)                     # Número de variables.
+        self.num_hol = self.num_holgura()               # Número de variables de holgura.
+        self.is_M, self.zj, self.tabla = self.matriz()  # Matriz de coeficientes.
+        self.c_v_var = self.c_v_variables()             # Datos de C y V.
+        self.imprimir()
+        
+    def num_holgura(self):
+        """Obtiene el número de variables total."""
+        cant = self.num_var + self.num_res              # Para cada restricción tiene una variable de holgura.
+        for res in self.restricciones: 
+            if res._var_H == [-1, 1]:                   # Método de la gran M.
+                cant += 1
+        return cant                                     # Retorna el número de variables de holgura.
+
+    def matriz(self):
+        """Obtiene una matriz de coeficientes."""
+        self.var = ['C', 'V', 'B'] + ['x_'+str(i+1) for i in range(self.num_var)]
+        self.identidad = []
+        s, r = 1, 1
+        is_M = False                                                            # Si se va a desarrollar con el método de la gran M.
+        matriz = []                                                             # Matriz.                       
+        zj = [0] + self.fo.coef                                                 # Zj.
+        self.fo.coef = [0] + self.fo.coef
+        count = 0                                                               # Método de la gran M.
+        for i in (range(self.num_res)):                                         # Coeficientes de las restricciones.
+            aux, hol = self.restricciones[i].holgura(i+count, self.num_hol + 1) # Coeficientes de las variables de Holgura.
+            if hol[1] == 1:  
+                aux[self.num_var+i+count+1] = 1
+                self.var.append('R_'+str(r))
+                self.identidad.append('R_'+str(r))
+                r+=1
+                if self.fo.max:
+                    zj.append(Symbol('M') * -1)                                  # Método de la gran M maximizar.
+                else:
+                    zj.append(Symbol('M'))                                       # Método de la gran M minimizar.
+                if hol[0] == -1:
+                    count +=1
+                    zj.append(0)  
+                    self.var.append('S_'+str(s))
+                    s+=1
+                    aux[self.num_var+i+count+1] = -1                             # Matriz identidad.
+                is_M = True
+            else:
+                zj.append(0)                                                     # Zj Coeficiente variable de holgura.
+                self.var.append('S_'+str(s))
+                self.identidad.append('S_'+str(s))
+                s+=1
+                self.fo.coef.append(0)
+                aux[self.num_var+i+count+1] = 1
+            matriz.append(aux)                                                   # Matriz identidad.
+            self.restricciones[i].coef = aux                                     # Coeficientes más holgura.
+        if is_M:
+            if self.fo.max:
+                zj = [sum(x) for x in zip(zj, self.Z())]                             # Coeficientes M despejada.
+            else:
+                aux = self.Z()
+                for i in range(len(aux)):
+                    zj[i] -= aux[i]
+            self.fo.coef = zj                                                   # Igualar los coeficientes a la función objetivo.
+        else:
+            for i in range(len(zj)):
+                zj[i] *= -1
+        return is_M, zj, matriz                                                  # Retorna la matriz de coeficientes.
+
+    def c_v_variables(self, columna=0, fila=0):
+        '''Obtiene los valores actualizados de C y V.'''
+        _c = []                                                             # Coeficiente.
+        _v = []                                                             # Variable.
+        if columna == 0:
+            for i in self.identidad:
+                _v.append(i)                                                # Variables de Holgura.
+                _c.append(0)                                                # Coeficientes en 0.
+        else:
+            _c = self.c_v_var[0]                                            # Coeficientes.
+            _v = self.c_v_var[1]                                            # Variables de Holgura.
+            _c[fila] = self.fo.coef[columna]                                    # Cambio de coeficiente.
+            _v[fila] = self.var[columna + 2]                                       # Cambio de variable.
+        return [_c, _v]                                                     # C | V.
+
+    def Cj_Zj(self):
+        '''Obtiene los valores de Cj - Zj.'''
+        cant = self.num_hol +1                                              # Total de variables.
+        zj = [0 for i in range(cant)]                                       # Zj.
+        cj = [0 for i in range(cant)]                                       # Cj.
+        for i in range(cant):
+            for j in range(self.num_res):
+                res = self.restricciones[j]
+                zj[i] += res.coef[i] * self.c_v_var[0][j]               # Zj (sumatoria de Aj * Cj).
+            cj[i] = zj[i] - self.fo.coef[i]                                  # Cj - Zj.
+        self.zj = cj
+        return self.zj
+    
+    def imprimir(self):
+        print("#######################################################")
+        print("#") 
+        print('#', ['Cj'], self.fo.coef)
+        for i in range(self.num_res):
+            print('#', self.c_v_var[0][i], self.c_v_var[1][i], self.tabla[i])
+        print('#', ['Zj'], self.zj)
+        print("#") 
+        print("#######################################################")
+
+    def operar(self):
+        print(self.pivote)
+        if self.pivote[1] != 0:
+            res = self.restricciones[self.pivote[0]]
+            res.operDiv(res.coef[self.pivote[1]])
+            for i in range (self.num_res):
+                if i != self.pivote[0]:
+                    self.restricciones[i].oper(self.pivote[1], res)
+                self.tabla[i] = self.restricciones[i].coef
+            self.c_v_variables(columna=self.pivote[1], fila=self.pivote[0])
+            if self.Cj_Zj() == True:
+                print('No tiene solución.')
+            self.pivote[1] = self.columna_pivote()
+            self.imprimir()
+            return False
+        else:
+            print("#######################################################")
+            print("#") 
+            print("# La solución más óptima es:")
+            if self.is_M:
+                aux = Poly(self.zj[0], Symbol('M'))
+                self.z = aux.coeffs()[1:][0]
+                print("# Z = " + str(aux.coeffs()[1:][0]))
+            else:
+                self.z = self.zj[0]
+                print("# Z = " + str(self.zj[0]))
+            for i in range(self.num_res):
+                if self.c_v_var[0][i] != 0:
+                    print("# " + self.c_v_var[1][i] + " = " + str(self.tabla[i][0]))
+            print("#") 
+            print("#######################################################")
+            return True
+
+    def columna_pivote(self):
+        '''Obtien la posición del valor a Máximizar o Minimizar.'''
+        aux = 0
+        pos = 0
+        if self.is_M:
+            for i in range(1, len(self.zj)):
+                try:
+                    if aux > self.zj[i].coeff(Symbol('M')) and self.zj[i].coeff(Symbol('M')) < 0 and self.fo.max:
+                        pos = i                                         # Obtener posición.
+                        aux = self.zj[i].coeff(Symbol('M'))             # Método de la Gran M.
+                    elif aux < self.zj[i].coeff(Symbol('M')) and self.zj[i].coeff(Symbol('M')) > 0 and not self.fo.max:                        
+                        pos = i                                         # Obtener posición.
+                        aux = self.zj[i].coeff(Symbol('M'))             # Método de la Gran M.
+                except:
+                    pass
+        else:
+            for i in range(1, len(self.zj)):                                # Recorre Zj - Cj.
+                if aux > self.zj[i] and self.zj[i] < 0 and self.fo.max:
+                    pos = i                                         # Obtener posición.
+                    aux = self.zj[i]
+                elif aux < self.zj[i] and self.zj[i] > 0 and not self.fo.max:
+                    pos = i                                         # Obtener posición.
+                    aux = self.zj[i]
+        self.pivote[1] = pos
+        return pos
+
+    def fila_pivote(self):
+        '''Obtien la posición del valor a Máximizar o Minimizar.'''
+        pos = 0
+        for i in range(self.num_res):
+            aux = self.restricciones[i].teta(self.pivote[1])
+            if aux != 0:
+                pos = i
+                break
+        for i in range(self.num_res):
+            teta = self.restricciones[i].teta(self.pivote[1])
+            if aux > teta and teta > 0:
+                aux = teta
+                pos = i
+        self.pivote[0] = pos
+        return pos
+
+    def Z(self):
+        """Obtiene los coeficientes de cada variable con el método de la gran M."""
+        cj = [0 for i in range(self.num_hol+1)]                       # Arreglo de coefientes.
+        for res in self.restricciones:
+            if res.getHolgura()[1] == 1:                            # Método de la gran M.
+                for i in range(self.num_hol):
+                    if self.fo.max:
+                        cj[i] += res.coef[i] * Symbol('M') * -1
+                    else:
+                        cj[i] += res.coef[i] * Symbol('M')
+        return cj                                                   # Retorna los coeficientes despejados.
---
+++ __init__.py
@@ -0,0 +1,226 @@
+import re
+from sympy import Rational
+from tkinter import messagebox, ttk
+from tkinter import *
+from FO import FO
+from Restriccion import Restriccion
+from Tabla import Matriz
+
+class MethodSimplex():
+    def __init__(self):
+        self.restricciones=[]
+        self.__raiz = Tk()
+        self.__raiz.title("Method Simplex")
+        self.__frame = Frame(self.__raiz,width="1000", height="600")
+        self.vista_1()
+        self.__raiz.mainloop()
+
+    def vista_1(self):
+        self.clear()
+        self.fo = []
+        self.res = []
+        self.igu = []
+        self.__frame.pack()
+        self.numVar = StringVar()
+        self.numRes = StringVar()
+
+        Label(self.__frame, text="METHOD SIMPLEX").grid(pady=15, columnspan=2)
+
+        Label(self.__frame, text="Ingrese el número de variables:").grid(row=1, column=0)
+        Entry(self.__frame, textvariable=self.numVar).grid(row=1, column=1)
+        
+        Label(self.__frame, text="Ingrese el número de restricciones:  ").grid(row=2, column=0)
+        Entry(self.__frame, textvariable=self.numRes).grid(row=2, column=1)
+
+        _btnOk=Button(self.__frame, text="OK", command=self.btnOk)
+        _btnOk.grid(pady=15, columnspan=2)
+        _btnOk.config(width=15)
+        
+        return self.numVar, self.numRes
+
+    def vista_2(self):
+        self.clear()
+
+        self.fo = [StringVar() for i in range(self.numVar)]
+        self.res = [[StringVar() for i in range(self.numVar)] for i in range(self.numRes)]
+        self.igu = [[StringVar() for i in range(2)] for i in range(self.numRes)]
+
+        Label(self.__frame, text="METHOD SIMPLEX").grid(pady=15, columnspan=self.numVar*2+2)
+        Label(self.__frame, text="Función objetivo:").grid(pady=15, row = 1,columnspan=self.numVar*2+2)
+
+        self.max_min = ttk.Combobox(self.__frame, width=6)
+        self.max_min['values'] = ('Máx Z', 'Min Z')
+        self.max_min.grid(row=2, column=0)
+        self.max_min.current(0)
+        Label(self.__frame, text="=").grid(row=2, column=1)
+
+        colum=2
+        for i in range(self.numVar):
+            aux = Entry(self.__frame, textvariable=self.fo[i])
+            aux.grid(row=2, column=colum)
+            aux.config(width=10)
+            colum+=1
+            Label(self.__frame, text="X_"+str(i+1)).grid(row=2, column=colum)
+            colum+=1
+
+        Label(self.__frame, text="Sujeto a:").grid(pady=15, row = 3,columnspan=self.numVar*2+2)
+
+        for i in range(self.numRes):
+            colum=0
+            for j in range(self.numVar):
+                aux = Entry(self.__frame, textvariable=self.res[i][j])
+                aux.grid(row=i+4, column=colum)
+                aux.config(width=10)
+                colum+=1
+                Label(self.__frame, text="X_"+str(j+1)).grid(row=i+4, column=colum)
+                colum+=1
+            self.igu[i][0] = ttk.Combobox(self.__frame, width=3)
+            self.igu[i][0]['values'] = ('<=', '=', '>=')
+            self.igu[i][0].grid(row=i+4, column=colum)
+            self.igu[i][0].current(0)
+            aux = Entry(self.__frame, textvariable=self.igu[i][1])
+            aux.grid(row=i+4, column=colum+1)
+            aux.config(width=10)
+        
+        aux = ""
+        for i in range(self.numVar):
+            aux += "X_" + str(i)
+            if i +1 == self.numVar:
+                aux += " >= 0"
+            else:
+                aux += ", "
+
+        Label(self.__frame, text=aux).grid(row=self.numRes+4, columnspan=self.numVar*2+2)
+
+        colum = int((self.numVar*2+2)/2)
+
+        _btnOk=Button(self.__frame, text="Atras", command=self.vista_1)
+        _btnOk.grid(pady=15, row=self.numRes+5, columnspan=colum, sticky=E)
+        _btnOk.config(width=8)
+
+        _btnOk=Button(self.__frame, text="Siguiente", command=self.btnV_2)
+        _btnOk.grid(pady=15, row=self.numRes+5, columnspan=colum, column=colum,sticky=W)
+        _btnOk.config(width=8)
+
+        return self.fo, self.res, self.igu
+
+    def vista_3(self):
+        self.clear()
+
+        pivote = [self.__tabla.columna_pivote(), self.__tabla.fila_pivote()]
+        print(pivote)
+
+        Label(self.__frame, text="METHOD SIMPLEX").grid(pady=15, columnspan=len(self.__tabla.fo.coef)+2, sticky=NSEW)
+        Label(self.__frame, relief=RIDGE, text="Cj").grid(row=2, column=1, sticky=NSEW)
+
+        for i in range(len(self.__tabla.fo.coef)):
+            if i == pivote[0] and pivote[0] != 0:
+                Label(self.__frame, relief=RIDGE, text=str(self.__tabla.fo.coef[i]), bg="blue").grid(row=2, column=i+2, sticky=NSEW)
+            else:
+                Label(self.__frame, relief=RIDGE, text=str(self.__tabla.fo.coef[i])).grid(row=2, column=i+2, sticky=NSEW)
+            self.__raiz.update()
+        
+        for i in range(self.__tabla.num_hol + 3):
+            if i == pivote[0] + 2 and pivote[0] != 0:
+                Label(self.__frame, relief=RIDGE, text=str(self.__tabla.var[i]), bg="blue").grid(row=3, column=i, sticky=NSEW)
+            else:
+                Label(self.__frame, relief=RIDGE, text=str(self.__tabla.var[i])).grid(row=3, column=i, sticky=NSEW)
+            self.__raiz.update()
+
+        for i in range(self.__tabla.num_res):
+            Label(self.__frame, relief=RIDGE, text=str(self.__tabla.c_v_var[0][i])).grid(row=i+4, column=0, sticky=NSEW)
+            Label(self.__frame, relief=RIDGE, text=str(self.__tabla.c_v_var[1][i]), bg="yellow").grid(row=i+4, column=1, sticky=NSEW)
+            for j in range(len(self.__tabla.tabla[i])):
+                if j == pivote[0] or i == pivote[1] and pivote[0] != 0:
+                    Label(self.__frame, relief=RIDGE, text=str(self.__tabla.tabla[i][j]), bg="blue").grid(row=i+4, column=j+2, sticky=NSEW)
+                else:
+                    Label(self.__frame, relief=RIDGE, text=str(self.__tabla.tabla[i][j])).grid(row=i+4, column=j+2, sticky=NSEW)
+                self.__raiz.update()
+        
+        Label(self.__frame, relief=RIDGE, text="Zj").grid(row=i+5, column=1, sticky=NSEW)
+        for j in range(len(self.__tabla.zj)):
+            if pivote[0] == 0 and j == 0:
+                Label(self.__frame, relief=RIDGE, text=str(self.__tabla.zj[j]), bg="blue").grid(row=i+5, column=j+2, sticky=NSEW)
+            elif j == pivote[0]:
+                Label(self.__frame, relief=RIDGE, text=str(self.__tabla.zj[j]), bg="blue").grid(row=i+5, column=j+2, sticky=NSEW)
+            else:
+                Label(self.__frame, relief=RIDGE, text=str(self.__tabla.zj[j])).grid(row=i+5, column=j+2, sticky=NSEW)
+            self.__raiz.update()
+
+        colum = int((len(self.__tabla.fo.coef)+3)/2)
+
+        _btnOk=Button(self.__frame, text="Atras", command=self.vista_1)
+        _btnOk.grid(pady=15, row=self.numRes+5, columnspan=colum, sticky=E)
+        _btnOk.config(width=8)
+
+        _btnOk=Button(self.__frame, text="Siguiente", command=self.operar)
+        _btnOk.grid(pady=15, row=self.numRes+5, columnspan=colum, column=colum,sticky=W)
+        _btnOk.config(width=8)
+
+    def btnV_2(self):
+        sw = True
+        expresion = r'^(([0-9]+)|([0-9]+[\.][0-9]+)|([0-9]+[\/][1-9][0-9]*))$'
+        for aux in self.fo:
+            if False == re.search(expresion, aux.get()):
+               sw = False 
+               break
+        for i in self.res:
+            for j in i:
+                if not re.search(expresion, j.get()):
+                    sw = False 
+                    break
+        for aux in range(self.numRes):
+            if not re.search(expresion, self.igu[aux][1].get()):
+               sw = False 
+               break
+        if sw:
+            fo = []
+
+            for aux in self.fo:
+                fo.append(Rational(aux.get()))
+
+            for i in range(self.numRes):
+                res = []
+                for aux in self.res[i]:
+                    res.append(Rational(aux.get()))
+                self.restricciones.append(Restriccion(res, self.igu[i][0].get(), Rational(self.igu[i][1].get())))
+
+            if self.max_min.current() == 0:
+                sw=True
+            else:
+                sw=False
+
+            self.__FO = FO(fo, sw)
+            self.__tabla = Matriz(self.__FO, self.restricciones)
+            self.vista_3()
+        else:
+            messagebox.showinfo("ERROR", "Hay algún número no valido.")
+
+    def operar(self):
+        if not self.__tabla.operar():
+            self.vista_3()
+        else:
+            aux = "\nZ = "+str(self.__tabla.z)
+            for i in range(self.__tabla.num_res):
+                if 'X_' in self.__tabla.c_v_var[1][i]:
+                    aux += "\n" + self.__tabla.c_v_var[1][i] + " = " + str(self.__tabla.tabla[i][0])
+            messagebox.showinfo("Solución", "La solución más óptima es:" + aux)
+
+    def btnOk(self):
+        if re.search(r'^[0-9]+$', self.numVar.get()) and re.search(r'^[0-9]+$', self.numRes.get()):
+            self.numVar, self.numRes = int(self.numVar.get()), int(self.numRes.get())
+            self.vista_2()
+        else:
+            messagebox.showinfo("ERROR", "La expresión debe ser solo números.")
+
+    def clear(self):
+        for a in self.__frame.grid_slaves():
+            a.destroy()
+
+    def method_simplex(self):
+        fo = Entry(self.__frame)
+        fo.place()
+        self.__frame.update()
+        self.__raiz.mainloop()
+
+a = MethodSimplex()
